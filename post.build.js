885298998d73a917d501aa8b622ca9aad63a0762
zer01one <aastoliarov02@gmail.com>
2024-08-03 18:15:04 +0300

    https://github.com/zer01one/event-sourcing-util/issues/4

--- post.build.js
+++ post.build.js
@@ -5,24 +5,35 @@ const functionSeparator = '\n\n';
 const jsonOuputOPTS = [false, 4];
 const dist = path.join(__dirname, 'dist');
 
+const messageGlobalScript = `/* 
+This file is generated automatically!
+
+Any changes will be overwritten during the next build. 
+*/`;
+
 (async function main() {
     cleanPacakageJson(path.join(dist, 'package.json')); // Clear out excess 
 
     formTypedPackageJSON(path.join(dist, '_cjs'), 'commonjs'); // Form typed json for modules
     formTypedPackageJSON(path.join(dist, '_esm'), 'module'); // Form typed json for modules
 
-    formGlobalScript('./dist/_esm/event-sourcing-util.js', path.join(dist, 'global', 'event-sourcing-util.js')); // Form global script
+    formGlobalScript('./dist/_esm/event-sourcing-util.js', path.join(dist, '_global'), 'event-sourcing-util'); // Form global script
 })();
 
-async function formGlobalScript(fromRelativePath, to) {
+async function formGlobalScript(fromRelativePath, to, name) {
     const module = await import(fromRelativePath).catch(err => ({ err }));
     if (module.err) return console.error('Ошибка импорта скрипта: ', module.err);
 
-    const output = '/* Automatically generated */' + functionSeparator + Object.values(module).reduce((acc, curr) => acc += curr += functionSeparator, '');
+    const output = Object.values(module).reduce((acc, curr) => acc += curr += functionSeparator, '');
 
-    fs.mkdirSync(to.split(path.sep).slice(0, -1).join(path.sep), { recursive: true }); // Create directory
+    fs.mkdirSync(to, { recursive: true }); // Create directory
 
-    fs.writeFileSync(to, output, { encoding: 'utf-8' }); // Write to file
+    fs.writeFileSync(path.join(to, name) + '.js', messageGlobalScript + functionSeparator + output, { encoding: 'utf-8' }); // Write to file
+    fs.writeFileSync(path.join(to, name) + '.encapsulated.js', createCncapsulatedGlobalModule(output, Object.keys(module)), { encoding: 'utf-8' }); // Write encapsulated to file
+
+    function createCncapsulatedGlobalModule(output, returns) {
+        return messageGlobalScript + functionSeparator + 'const EventSourcingUtil = (() => {' + functionSeparator + output + functionSeparator + 'return { ' + returns + ' };' + functionSeparator + '})();';
+    }
 }
 
 function cleanPacakageJson(path) {
